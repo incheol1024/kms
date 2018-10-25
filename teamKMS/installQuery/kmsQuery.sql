41cd5e7433b3f258ca0aa3f73c0d2203dd89bd6f
Yuk <Yuk@DESKTOP-43F479E>
2018-10-26 00:27:50 +0900

    add table set utf-8 as default

--- teamKMS/installQuery/kmsQuery.sql
+++ teamKMS/installQuery/kmsQuery.sql
@@ -1,10 +1,11 @@
-/*오토커밋 꺼라 */
+/*create database [databaseName] DEFAULT CHARACTER SET utf8 collate utf8_general_ci;
+오토커밋 꺼라 */
 
 create table KMS.KMSMenu (
 	menu_id INT(11) unsigned auto_increment primary key,
 	menu_name VARCHAR(32) NOT NULL,
     menu_type VARCHAR(16) NOT NULL
-);
+) default character set utf8 collate utf8_general_ci ;
               
 insert into KMS.KMSMenu(menu_name,menu_type) values('ECM','SOL');
 insert into KMS.KMSMenu(menu_name,menu_type) values('EDM','SOL');
@@ -30,7 +31,7 @@ create table KMS.KMSGroup (
     group_parent INT(11) unsigned,
 	FOREIGN KEY (group_parent)
     REFERENCES KMS.KMSGroup(group_id) ON UPDATE CASCADE
-);
+) default character set utf8 collate utf8_general_ci ;
 
 insert into KMS.KMSGroup(group_id,group_name) values(0,'ROOT');
 insert into KMS.KMSGroup(group_id,group_name,group_parent) values(1,'DefaultGroup',0);
@@ -46,7 +47,7 @@ create table KMS.KMSUser (
     user_extId varchar(32),
     FOREIGN KEY (user_group)
     REFERENCES KMS.KMSGroup(group_id) ON UPDATE CASCADE
-);
+) default character set utf8 collate utf8_general_ci ;
 
 #ADMIN , ADMIN
 insert into KMS.KMSUser(user_id,user_name,user_type,user_group,user_pass) values('ADMIN','ADMIN','ADMIN',1,'$2a$10$RWMKRWqLCWbjhIjiGzwvqusafXr8Y76JpB5SdaJCHoZeyAhb1aohu');
@@ -62,6 +63,6 @@ create table KMS.KMSCommonPost (
     post_category INT(11) unsigned,
     FOREIGN KEY (post_category)
     REFERENCES KMS.KMSMenu(menu_id) ON UPDATE CASCADE
-);
+) default character set utf8 collate utf8_general_ci ;
 
 CREATE INDEX FK_POST_CETE ON KMS.KMSCommonPost(post_category);
