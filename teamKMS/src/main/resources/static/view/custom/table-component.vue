7e42a6f4f23058990ccd82f0a2952c1b2b165069
yukyongsoo <yozx9079@naver.com>
2019-01-18 11:47:09 +0900

    공통 테이블 컴포넌트 작성 테스트 페이지 작성 spring.jpa.open-in-view=false 설정

--- teamKMS/src/main/resources/static/view/custom/table-component.vue
+++ teamKMS/src/main/resources/static/view/custom/table-component.vue
@@ -1,8 +1,8 @@
 <template>
     <v-data-table :headers="header" :items="dataset" :pagination.sync="pagination"
-                  :loading="loading" class="elevation-1">
+                  :total-items="total" :loading="loading" must-sort class="elevation-1">
         <template slot="items" slot-scope="props">
-            <td v-for="value in props.item">{{ value }}</td>
+            <td v-for="(value,propertyName) in props.item">{{ value }}</td>
         </template>
     </v-data-table>
 </template>
@@ -14,22 +14,23 @@
             pagefunction : Function
         },
         data: () => ({
-            pagination: { rowsPerPage : 0 , page : 0, descending : false, sortBy : ""},
+            pagination: {},
             loading: false,
             dataset : [],
+            total : 0
         }),
         watch: {
             pagination: {
                 handler() {
                     try{
-                        this.loading = true
-                        this.dataset = []
-                        var _this = this
+                        this.loading = true;
+                        this.dataset = [];
+                        var _this = this;
                         this.pagefunction(this.getSpringType(this.pagination)).then(function (response) {
-                            console.log(response)
-                            var max = response.data.length;
+                            _this.total = response.data.total
+                            var max = response.data.list.length;
                             for (var i = 0; i < max; i++) {
-                                _this.dataset.push(response.data[i]);
+                                _this.dataset.push(response.data.list[i]);
                             }
                         }).catch(function (error) {
                             alert(error);
@@ -42,8 +43,8 @@
         },
         methods: {
             getSpringType : function getSpringType(pagination) {
-                let str = "asc"
-                if(pagination.descending) str = "desc"
+                let str = "asc";
+                if(pagination.descending) str = "desc";
                 return {page: pagination.page , size : pagination.rowsPerPage, sort : pagination.sortBy + "," + str}
             }
         }
