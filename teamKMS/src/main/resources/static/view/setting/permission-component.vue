213107626efe35b966afca322f45bf7e8c1e51c3
yongsoo <yozx9079@naver.com>
2019-02-09 02:55:43 +0900

    UI 새로 구상해야 할것 같음

--- teamKMS/src/main/resources/static/view/setting/permission-component.vue
+++ teamKMS/src/main/resources/static/view/setting/permission-component.vue
@@ -14,29 +14,11 @@
 
         <v-flex>
             <v-card>
-                <h5 class="title mb-0">User/Group</h5>
+                <v-card-title class="title">Detail</v-card-title>
                 <v-layout>
-
                     <v-flex class="mr-3">
-                        <table-component :header="ugheader" :pagefunction="ugpage" :addfunction=null :deletefunction=null :editfunction=null :search=null></table-component>
+
                     </v-flex>
-
-                    <v-flex>
-                        <v-list>
-                            <v-list-tile v-for="value in acllist">
-                                <v-list-tile-action>
-                                    <v-checkbox></v-checkbox>
-                                </v-list-tile-action>
-
-                                <v-list-tile-content>
-                                    <v-list-tile-title>{{value.aclName}}</v-list-tile-title>
-                                    <v-list-tile-sub-title></v-list-tile-sub-title>
-                                </v-list-tile-content>
-                            </v-list-tile>
-                        </v-list>
-                        <v-btn color="primary">Confirm</v-btn>
-                    </v-flex>
-
                 </v-layout>
             </v-card>
         </v-flex>
@@ -45,27 +27,12 @@
 
 <script>
     module.exports = {
-        created: function created() {
-            let _this = this;
-            axios.get("acl", {
-                params : getJavaMaxPage()
-            }).then(value => {
-                let max = value.data.content.length;
-                for (let i = 0; i < max; i++) {
-                    _this.acllist.push(value.data.content[i]);
-                }
-            }).catch(reason => catchPromise(reason));
-        },
         data: () => ({
             aclheader: [
                 {text: 'aclId', value: 'aclId'},
                 {text: 'aclName', value: 'aclName'},
                 {text: "action", sortable : false}
-            ],
-            ugheader: [
-                {text: 'name', value: 'name'}
-            ],
-            acllist : []
+            ]
         }),
         methods: {
             aclpage: function aclpage(page) {
@@ -78,11 +45,6 @@
             },
             editacl: function editacl(item) {
                 return axios.post("acl" ,item)
-            },
-            ugpage: function ugpage(page) {
-                return axios.get("group/list",{
-                    params : page
-                })
             }
 
         }
