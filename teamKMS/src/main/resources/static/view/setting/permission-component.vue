7d5e887e5ea31959e5a7d7f7de31453156b8a578
yongsoo <yozx9079@naver.com>
2019-02-14 00:38:09 +0900

    ACL 화면 만드는중

--- teamKMS/src/main/resources/static/view/setting/permission-component.vue
+++ teamKMS/src/main/resources/static/view/setting/permission-component.vue
@@ -1,79 +1,82 @@
 <template>
-    <v-layout>
-        <v-flex>
-            <v-card>
-                <v-card-title>
-                    <div>
-                        <h3 class="headline mb-0">Acl List</h3>
-                        <v-list two-line>
-                            <template v-for="data in listData" :key="i">
-                                <v-list-tile @click="">
-                                    <v-list-tile-content>
-                                        <v-list-tile-title>{{data.aclId}}</v-list-tile-title>
-                                        <v-list-tile-sub-title>{{data.aclName}}</v-list-tile-sub-title>
-                                    </v-list-tile-content>
-                                </v-list-tile>
-                            </template>
-                        </v-list>
-                    </div>
-                </v-card-title>
-                <v-card-actions>
-                    <v-btn color="primary" @click="newAcl">New</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
+    <v-container fluid grid-list-lg>
+        <v-layout>
+            <v-flex align-space-around>
+                <v-card>
+                    <v-card-title>
+                        <v-layout wrap row>
+                            <h3 class="headline mb-0">Acl List</h3>
+                            <v-list two-line>
+                                <template v-for="data in listData" :key="i">
+                                    <v-list-tile @click="">
+                                        <v-list-tile-content>
+                                            <v-list-tile-title>{{data.aclId}}</v-list-tile-title>
+                                            <v-list-tile-sub-title>{{data.aclName}}</v-list-tile-sub-title>
+                                        </v-list-tile-content>
+                                    </v-list-tile>
+                                </template>
+                            </v-list>
+                        </v-layout>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn color="primary" @click="newAcl">New</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
 
-        <v-flex v-if="showform">
-            <v-card>
-                <v-card-title class="title">Edit Acl</v-card-title>
-                <v-layout>
-                    <v-form>
-                        <v-text-field label="Acl Id"></v-text-field>
-                        <v-text-field label="Acl Name"></v-text-field>
-                        <v-container fluid>
-                            <v-layout v-for="i in 3" :key="i">
-                                <v-checkbox v-model="selected" label="User" :value="i"></v-checkbox>
-                                <v-checkbox v-model="selected" label="Group" :value="i+3"></v-checkbox>
-                                <v-checkbox v-model="selected" label="Permission" :value="i+6"></v-checkbox>
-                                <v-checkbox v-model="selected" label="Qna" :value="i+9"></v-checkbox>
-                                <v-checkbox v-model="selected" label="SOL" :value="i+12"></v-checkbox>
-                                <v-checkbox v-model="selected" label="SITE" :value="i+15"></v-checkbox>
-                            </v-layout>
-                        </v-container>
-                    </v-form>
-                </v-layout>
-                <v-card-actions>
-                    <v-btn color="primary" @click="confirmAcl">Confirm</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-
-        <v-flex v-if="showlist">
-            <v-card>
-                <v-card-title class="title">User/Group List</v-card-title>
-                <v-layout>
-                    <table-component :header="header" :pagefunction="ugpage" :addfunction=null
-                                     :deletefunction="delacl" :editfunction="editacl" :search=null></table-component>
-                </v-layout>
-                <v-card-actions>
-                    <v-btn color="primary" @click="setList">Set</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-    </v-layout>
+            <v-flex>
+                <v-card>
+                    <v-tabs color="transparent">
+                        <v-tab>Edit Acl</v-tab>
+                        <v-tab>Set User/Group</v-tab>
+                        <v-tab-item>
+                            <v-card-title class="title">
+                                <v-layout wrap row>
+                                    <v-form>
+                                        <v-text-field label="Acl Id"></v-text-field>
+                                        <v-text-field label="Acl Name"></v-text-field>
+                                        <v-layout v-for="i in 3" :key="i">
+                                            <v-checkbox v-model="selected" label="User" :value="i"></v-checkbox>
+                                            <v-checkbox v-model="selected" label="Group" :value="i+3"></v-checkbox>
+                                            <v-checkbox v-model="selected" label="Permission" :value="i+6"></v-checkbox>
+                                            <v-checkbox v-model="selected" label="Qna" :value="i+9"></v-checkbox>
+                                            <v-checkbox v-model="selected" label="SOL" :value="i+12"></v-checkbox>
+                                            <v-checkbox v-model="selected" label="SITE" :value="i+15"></v-checkbox>
+                                        </v-layout>
+                                    </v-form>
+                                </v-layout>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn color="primary" @click="confirmAcl">Confirm</v-btn>
+                            </v-card-actions>
+                        </v-tab-item>
+                        <v-tab-item>
+                            <v-card-title class="title">
+                                <v-layout wrap row>
+                                    <table-component :header="header" :pagefunction="ugpage" :addfunction=null
+                                                     :deletefunction="delacl" :editfunction="editacl"
+                                                     :search=null></table-component>
+                                </v-layout>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn color="primary" @click="setList">Set</v-btn>
+                            </v-card-actions>
+                        </v-tab-item>
+                    </v-tabs>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     module.exports = {
         data: () => ({
-            showlist: true,
-            showform: true,
             listData: [],
             header: [
                 {text: 'name', value: 'name'},
                 {text: 'type', value: 'type'}
             ],
-
             selected: []
         }),
         created: function created() {
@@ -93,6 +96,7 @@
             setList: function setList(item) {
             },
             newAcl: function newAcl() {
+
 
             }
 
