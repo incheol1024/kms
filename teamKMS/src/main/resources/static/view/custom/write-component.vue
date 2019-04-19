8ab5262fd648f709dc1b928879ba4b82177067d8
yongsoo <yozx9079@naver.com>
2019-04-20 01:25:04 +0900

    write Component 작업

---
+++ teamKMS/src/main/resources/static/view/custom/write-component.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout column>
+    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" :disabled="readOnly"></ckeditor>
+    <v-btn color="primary" flat @click="save">Save</v-btn>
+  </v-layout>
+</template>
+
+<script>
+module.exports = {
+  props: {
+    editorData: '',
+    readOnly : false,
+    save: {
+      default: function (page) {
+        return new Promise(function (resolve, reject) {
+        })
+      },
+      type: Function
+    }
+  },
+  data: () => ({
+    editor: ClassicEditor,
+    editorConfig: {}
+  }),
+  methods: {
+
+  }
+};
+</script>
