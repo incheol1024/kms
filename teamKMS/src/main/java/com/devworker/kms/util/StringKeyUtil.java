4729a93b809b5b013a2e850ffc85bdc465949339
incheol <yaho102466@gmail.com>
2019-05-13 15:23:23 +0900

    1. pom.xml apache commonLang 의존성 제거, StringKeyUtil 코드 변경

--- teamKMS/src/main/java/com/devworker/kms/util/StringKeyUtil.java
+++ teamKMS/src/main/java/com/devworker/kms/util/StringKeyUtil.java
@@ -1,6 +1,6 @@
 package com.devworker.kms.util;
 
-import org.apache.commons.lang3.RandomStringUtils;
+import java.util.Random;
 
 public class StringKeyUtil {
 
@@ -8,12 +8,24 @@ public class StringKeyUtil {
 		return getTimeStamp() + makeRandomString(10);
 	}
 
-	private static String makeRandomString(int length) {
-		return RandomStringUtils.random(length, true, true);
-	}
-
 	private static String getTimeStamp() {
 		return System.currentTimeMillis() + "";
+	}
+
+	private static String makeRandomString(int length) {
+
+		int leftLimit = 97;
+		int rightLimit = 122;
+		int targetStringLength = length;
+		Random random = new Random();
+		StringBuilder buffer = new StringBuilder(targetStringLength);
+
+		for (int i = 0; i < targetStringLength; i++) {
+			int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+			buffer.append((char) randomLimitedInt);
+		}
+
+		return buffer.toString();
 	}
 
 }
--- teamKMS/src/test/java/com/devworker/kms/util/StringKeyUtilTest.java
+++ teamKMS/src/test/java/com/devworker/kms/util/StringKeyUtilTest.java
@@ -2,6 +2,7 @@ package com.devworker.kms.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,7 +12,7 @@ public class StringKeyUtilTest {
 
 	@Test
 	public void generateUniqueKeyTest() {
-		int testSize = 1000000;
+		int testSize = 10000;
 		for (int i = 0; i < testSize; i++) {
 			String key = StringKeyUtil.generateUniqueKey();
 			assertThat(key).doesNotContainAnyWhitespaces().hasSize(23);
@@ -20,13 +21,40 @@ public class StringKeyUtilTest {
 
 	@Test
 	public void isUniqueTest() {
-		int testSize = 1000000;
+		int testSize = 10000;
 		Set<String> keySet = new HashSet<>(testSize);
 		for (int i = 0; i < testSize; i++) {
 			String key = StringKeyUtil.generateUniqueKey();
 			assertThat(keySet.contains(key)).isFalse();
 			keySet.add(key);
 		}
+	}
+	
+	@Test
+	public void whatIsShapeOfKey() {
+			System.out.println(StringKeyUtil.generateUniqueKey());
+	}
+	
+	@Test
+	public void systemCurrentMillsSpeedTest() {
+		
+		long start = System.currentTimeMillis();
+		for(int i = 0; i < 100000000; i++) {
+			System.currentTimeMillis();
+		}
+		System.out.println("System.currentTimeMills() = " + String.valueOf(System.currentTimeMillis() - start));
+	}
+	
+	@Test
+	public void dateObjectSpeedTest() {
+		
+		long start = System.currentTimeMillis();
+		for(int i=0; i<100000000; i++) {
+			Date date = new Date();
+		}
+		
+		System.out.println("new Date() = " + String.valueOf(System.currentTimeMillis() - start));
+		
 	}
 
 }
