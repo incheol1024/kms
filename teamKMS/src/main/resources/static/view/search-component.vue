3876c586966a6649568ac1f74cffda22ae024239
yongsoo <yozx9079@naver.com>
2019-05-14 22:28:33 +0900

    이미지 수정 및 간격 조정

--- teamKMS/src/main/resources/static/view/search-component.vue
+++ teamKMS/src/main/resources/static/view/search-component.vue
@@ -1,35 +1,37 @@
 <template>
     <v-layout column>
-        <v-text-field label="Search Word" outline @keyup.enter="searchDoc" v-model="search"></v-text-field>
-        <v-card v-for="data in searchData">
-            <v-card-title primary-title>
-                <div>
-                    <div class="headline">Unlimited music now</div>
-                    <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
-                </div>
-            </v-card-title>
-            <v-card-actions>
-                <v-btn flat>Listen now</v-btn>
-            </v-card-actions>
-        </v-card>
+        <v-flex v-for="data in searchData" :key="data.id">
+            <v-card >
+                <v-card-title primary-title>
+                    <div>
+                        <div class="headline">Unlimited music now</div>
+                        <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
+                    </div>
+                </v-card-title>
+                <v-card-actions>
+                    <v-btn flat>Listen now</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-flex>
 
-        <v-card>
-            <v-card-title primary-title>
-                <div>
-                    <div class="headline"><a href="asfasf">TEST1</a></div>
-                    <span>위 문서는 우짜구 저짜구</span>
-                </div>
-            </v-card-title>
-            <v-card-actions>
-            </v-card-actions>
-        </v-card>
+        <v-flex>
+            <v-card>
+                <v-card-title primary-title>
+                    <div>
+                        <div class="headline"><a href="asfasf">TEST1</a></div>
+                        <span>위 문서는 우짜구 저짜구</span>
+                    </div>
+                </v-card-title>
+                <v-card-actions>
+                </v-card-actions>
+            </v-card>
+        </v-flex>
     </v-layout>
 </template>
 
 <script>
     module.exports = {
         data: () => ({
-            search: "",
             searchData: []
         }),
         methods: {
